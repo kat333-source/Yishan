8d5580794af9e85da1dc853e58d2c62f16afb405
kat333-source <2650452820@qq.com>
2024-06-07 11:39:18 +0200

    pro and cons of git

--- Yishan Ding(12818522)&amp;Siyu Bai(12703653).R
+++ Yishan Ding(12818522)&amp;Siyu Bai(12703653).R
@@ -4,3 +4,11 @@ author: "Yishan Ding(12818522)&amp;Siyu Bai(12703653)"
 date: "2024-05-23"
 output: html_document
 ---
+  
+  ## Exercise 1: Initializing git
+  
+  # d) Name 2 strengths and 2 weaknesses of git
+strengths <- c("1. Distributed version control system",
+                 "2. Efficient handling of large projects")
+weaknesses <- c("1. Steeper learning curve for beginners",
+                "2. Potential for merge conflicts in collaborative projects")
